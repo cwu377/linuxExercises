f9e5f3153f4a7146df557313b008c70274dbaa35
Chao-Sheng <cwu377@wisc.edu>
2022-10-22 00:52:32 -0500

    exercise 7 update

--- mean.sh
+++ mean.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 F=$2
 F_cutted=$(cut -d, -f $1 $F)
-echo $F_cutted
+F_skip_header=$(echo "$F_cutted" | tail)
+#echo $F_skip_header
+echo "$F_skip_header" | awk '{ total += $1 } END { print total/NR }'
