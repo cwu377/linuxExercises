95e03fe7d5c01f2fb59ccbcb85e6cf8ee50b7cf8
ShichenGao <114317809+ShichenGao@users.noreply.github.com>
2023-03-09 23:19:29 -0600

    Update school.sh

--- school.sh
+++ school.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 F=Property_Tax_Roll.csv
-head -n 3 $F
 filtered_F=$(grep -e 'MADISON SCHOOLS' $F)
 cutted_F=$(echo $filtered_F | cut -d, -f 7)
 echo "$filtered_F" | awk '{ total += $7 } END { print total/NR }' 
