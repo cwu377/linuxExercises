2dc4d6ed88cc3d1c465335dc1b6d17b796da7a9c
Chao-Sheng <cwu377@wisc.edu>
2023-03-09 22:10:50 -0500

    fix cutting the wrong column

--- mean.sh
+++ mean.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 F=$2
-F_cutted=$(cut -d, -f $1 $F)
+col=$1
+F_cutted=$(cut -d, -f $((col+1)) $F)
 F_skip_header=$(echo "$F_cutted" | tail)
 #echo $F_skip_header
 echo "$F_skip_header" | awk '{ total += $1 } END { print total/NR }'
